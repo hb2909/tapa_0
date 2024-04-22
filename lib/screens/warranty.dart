9771cd58b8fc4eaf5eaba2ce3799802006207914
Carmen Lau <carmenlkw2@gmail.com>
2024-04-22 09:28:41 +0800

    Warranty Screen

--- lib/screens/warranty.dart
+++ lib/screens/warranty.dart
@@ -1,12 +1,74 @@
 import 'package:flutter/material.dart';
 
 class WarrantyScreen extends StatelessWidget {
-  const WarrantyScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Warranty'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: Text('Warranty'),
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 20.0,
+        ),
+        centerTitle: true,
+      ),
+        body: 
+          SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Coverage',
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              'This warranty covers TAPA-0 hardware device and its accompanying products and sensors.',
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'Warranty Period',
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              'The warranty is valid for one (1) year from the date of purchase.',
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'What\'s Covered',
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              'This warranty covers device manufacturing defects and any malfunctions in its material and workmanship.',
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'What\'s Not Covered',
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              'The warranty does not cover damage caused by:\n'
+              '\t\t\t• \t\tPhysical abuse or neglect.\n'
+              '\t\t\t• \t\tImproper installation or use.\n'
+              // '\t\t\t• \t\tNatural Disasters (e.g., lightning strike, landslides)\n',
+              '\t\t\t• \t\tIntentional damage to the device. \n',
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'Warranty Service Process',
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              'To make a warranty claim, please contact us at support@tapa0.com.',
+            ),
+          ],
+        ),
       ),
     );
   }
